2b0bc3b47c76b690c2151b80ae893b30d151953d
AsiaGrpr <groupierre.asia@gmail.com>
2023-01-20 03:28:24 -0800

    6-print_comb3.py correction pep8

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,5 @@
 for i in range(8):
     for j in range(i+1, 10):
         if i != j:
-            print("{}{}, ".format(i, j), end = "")
+            print("{}{}, ".format(i, j), end="")
 print('{}'.format(89))
