dd7f764ee433de9cbc720040ed5ba5a10c8e227f
AsiaGrpr <groupierre.asia@gmail.com>
2023-01-20 03:27:34 -0800

    6-print_comb3.py correction output

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(9):
+for i in range(8):
     for j in range(i+1, 10):
         if i != j:
-            if i != 8 and j != 9:
-                print("{}{}, ".format(i, j), end = "")
+            print("{}{}, ".format(i, j), end = "")
 print('{}'.format(89))
