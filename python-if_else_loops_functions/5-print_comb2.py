5e18adc3c6a1e97a2bf685f64b1e25320e4bcb18
AsiaGrpr <groupierre.asia@gmail.com>
2023-01-20 03:13:28 -0800

    5-print_comb2.py was mixed with the task 6

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-for i in range(9):
-    for j in range(i+1, 10):
-        if i != j:
-            if i != 8 and j != 9:
-                print("{}{}, ".format(i, j), end = "")
-print('{}'.format(89))
+for number in range(0, 99):
+    print('{:02}'.format(number), end=', ')
+print('{}'.format(99))
