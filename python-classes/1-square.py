212efa4f95733c9bbacfc916fe8e57a573b069bd
AsiaGrpr <groupierre.asia@gmail.com>
2023-01-30 14:08:05 -0800

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''module to create a square with the private instance attribute size added'''
+
+
+class Square:
+    '''class with size attribute'''
+
+    def __init__(self, size):
+        self.__size = size
