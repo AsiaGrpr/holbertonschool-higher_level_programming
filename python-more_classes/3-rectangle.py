5466c14cd2de26f1e87690f5c8611464b4285cc8
Asia Groupierre <113519051+AsiaGrpr@users.noreply.github.com>
2023-02-01 22:18:26 +0100

    Update 3-rectangle.py

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -45,7 +45,7 @@ class Rectangle:
     def __str__(self):
         string = ""
         if self.__height == 0 or self.__width == 0:
-            return string
+            return (string)
         for i in range(self.__height):
             for j in range(self.__width):
                 string += "#"
