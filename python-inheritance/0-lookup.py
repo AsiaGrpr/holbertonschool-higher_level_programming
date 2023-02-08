b833c9c0c57ccc620212b9603809592b4c4e9770
AsiaGrpr <groupierre.asia@gmail.com>
2023-02-08 01:27:02 -0800

    0-lookup.py correction documentation

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+'''module to print list of attribute of an object'''
+
+
 def lookup(obj):
     '''function that returns the list of
     available attributes and methods of an object'''
