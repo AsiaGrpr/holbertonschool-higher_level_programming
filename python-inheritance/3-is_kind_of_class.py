1587c4915b6fffd9957f3a978e3c2f637fca5003
AsiaGrpr <groupierre.asia@gmail.com>
2023-02-08 02:13:52 -0800

    3-is_kind_of_class.py

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''module to define if he object is an instance of a class or inherited class'''
+
+
+def is_kind_of_class(obj, a_class):
+    ''' function that returns True if the object is an instance of, or if
+    the object is an instance of a class that inherited from,
+    the specified class ; otherwise False'''
+
+    return (isinstance(obj, a_class))
