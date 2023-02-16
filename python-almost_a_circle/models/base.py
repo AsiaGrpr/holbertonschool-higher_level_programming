60f928cecaefddac7c43115f74ef15e8d678987f
AsiaGrpr <groupierre.asia@gmail.com>
2023-02-16 06:36:06 -0800

    models/base.py task16

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -27,3 +27,17 @@ class Base():
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''class method that writes the JSON string
+        representation of list_objs to a file'''
+
+        new = []
+        filename = cls.__name__ + ".json"
+        if list_objs is not None:
+            for i in list_objs:
+                new.append(i.to_dictionary())
+        json_string = cls.to_json_string(new)
+        with open(filename, 'w', encoding="utf-8") as f:
+            f.write(json_string)
