fd57dcd98be91447f193e5f0af149a58f261d4bf
AsiaGrpr <groupierre.asia@gmail.com>
2023-02-16 00:46:50 -0800

    models/square.py task10

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+'''module And now, the Square! with class inherited: parent = Rectangle'''
+
+
+from models.base import Base
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    '''class Square that inherits from Rectangle'''
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        return (self.width)
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self.width = value
+            self.height = value
+        else:
+            raise TypeError('width must be an integer')
+        if value <= 0:
+            raise ValueError('width must be > 0')
+
+    def __str__(self):
+        '''method should return [Square] (<id>) <x>/<y> - <size>'''
+        return ("[Square] ({}) {}/{} - {}".
+                format(self.id, self.x, self.y,
+                       self.width))
