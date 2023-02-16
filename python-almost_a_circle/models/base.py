432bf1bc364800537515b5833125534231f21f9e
AsiaGrpr <groupierre.asia@gmail.com>
2023-02-16 01:56:29 -0800

    models/base.py task15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,9 @@
 '''models module'''
 
 
+import json
+
+
 class Base():
     '''class base'''
 
@@ -14,4 +17,13 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-    pass
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''method that returns the JSON string
+        representation of list_dictionaries'''
+
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
