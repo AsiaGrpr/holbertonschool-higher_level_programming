418dc7137de781ba1519bbdf3da9cf0aa28b182e
AsiaGrpr <groupierre.asia@gmail.com>
2023-02-16 01:23:19 -0800

    models/rectangle.py task13

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -105,3 +105,9 @@ class Rectangle(Base):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        '''method that returns the dictionary representation of a Rectangle'''
+
+        return dict(id=self.id, width=self.__width, height=self.__height,
+                    x=self.__x, y=self.__y)
