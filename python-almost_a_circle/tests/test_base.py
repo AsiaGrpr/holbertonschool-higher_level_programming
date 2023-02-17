5b63f85669fb986f3132a29f8f4dc4ea2b9e3e03
AsiaGrpr <groupierre.asia@gmail.com>
2023-02-17 07:45:01 -0800

    test_base test2

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -18,3 +18,36 @@ class TestBase(unittest.TestCase):
         #if ID is not None, ID == value
         base = Base(89)
         self.assertEqual(base.id, 89)
+
+    def test_5(self):
+        Base.to_json_string([])
+
+    def test_6(self):
+        Base.save_to_file([])
+
+    def test_7(self):
+        Base.save_to_file(None)
+
+    def test_8(self):
+        Base.to_json_string([])
+
+    def test_9(self):
+        Base.to_json_string([ { 'id': 12 }])
+
+    def test_10(self):
+        Base.from_json_string(None)
+
+    def test_11(self):
+        Base.from_json_string("[]")
+
+    def test_12(self):
+        Base.from_json_string("[]")
+
+    def test_13(self):
+        Base.from_json_string('[{ "id": 89 }]')
+
+    def test_14(self):
+        Base.from_json_string('[{ "id": 89 }]')
+
+if __name__ == '__main__':
+    unittest.main()
