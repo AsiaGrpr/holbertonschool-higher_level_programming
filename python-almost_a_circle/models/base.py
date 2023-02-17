d65a15115670a4120f859194a40d1f3f60a7442c
AsiaGrpr <groupierre.asia@gmail.com>
2023-02-17 02:17:17 -0800

    models/base.py task17 to 19

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -41,3 +41,33 @@ class Base():
         json_string = cls.to_json_string(new)
         with open(filename, 'w', encoding="utf-8") as f:
             f.write(json_string)
+
+    @staticmethod
+    def from_json_string(json_string):
+        '''method that returns the list of the JSON string
+        representation json_string'''
+
+        if json_string is not None or json_string:
+            return json.loads(json_string)
+        return []
+
+    @classmethod
+    def create(cls, **dictionary):
+        '''method that returns an instance with all attributes already set'''
+
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        if cls.__name__ == "Square":
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, 'r') as f:
+                return [cls.create(**dict) for
+                        dict in cls.from_json_string(f.read())]
+        except FileNotFoundError:
+            return []
