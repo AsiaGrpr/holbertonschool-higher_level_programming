25c93004f4ddb8c407a40935d827f6b912b0be05
AsiaGrpr <groupierre.asia@gmail.com>
2023-04-11 12:46:23 -0700

    10-factorial.js

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const process = require('process');
+const argv = process.argv;
+
+function factorialize (num) {
+  if (num < 0) { return -1; } else if (num == 0) { return 1; } else {
+    return (num * factorialize(num - 1));
+  }
+}
+
+if (isNaN(argv[2]) !== true) {
+  console.log(factorialize(Number(argv[2])));
+} else {
+  console.log(1);
+}
